e6e13fcd191c4e9c75edc2e349c120a78c22b410
itamars1000 <143691745+itamars1000@users.noreply.github.com>
2023-09-11 11:50:17 +0300

    step2

--- game_card/Player.py
+++ game_card/Player.py
@@ -1,2 +1,34 @@
+from DeckOfCard import DeckOfCards
+from random import choice
+from Card import Card
+
+
+# The class Player includes data(name, number of card,list of cards) about the cards deck and the following Methods:
+# set_hand
+# get_card
+# add_card
+
+
 class Player:
-    pass
+    def __init__(self, name, numb_cards=26):
+        """define the name player and number of cards and the list of cards"""
+        self.name = name
+        self.numb_cards = numb_cards
+        if self.numb_cards > 26 or self.numb_cards < 10:
+            self.numb_cards = 26
+        self.list_cards = []
+
+    def set_hand(self, cards_deck: DeckOfCards):
+        """function divide random cards to the player"""
+        for i in range(self.numb_cards):
+            self.list_cards.append(cards_deck.deal_one())
+
+    def get_card(self):
+        """choose random card from the list of cards"""
+        card = choice(self.list_cards)
+        self.list_cards.remove(card)
+        return card
+
+    def add_card(self, card: Card):
+        """function will get a card and will add it to the list of cards"""
+        self.list_cards.append(card)
