0346592190f04ffe179dbaeacc2926f507d4cdce
itamars1000 <143691745+itamars1000@users.noreply.github.com>
2023-09-11 15:44:46 +0300

    Card tests

--- game_card/Card.py
+++ game_card/Card.py
@@ -7,7 +7,11 @@ class Card:
     def __init__(self, value, suit):
         """Defines new card: value and suit"""
         self.value = value
+        if self.value < 1 or self.value > 13:
+            raise ValueError("Value must be between 1-13")
         self.suit = suit
+        if self.suit < 1 or self.suit > 4:
+            raise ValueError("Suit must be between 1-4")
 
     def __repr__(self):
         list_values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
@@ -16,6 +20,8 @@ class Card:
 
     def __gt__(self, other):
         """Checks if the card higher than other card"""
+        if type(other) != Card:
+            raise TypeError("Other must be a Card")
         if self == other:
             if self.suit > other.suit:
                 return True
@@ -37,6 +43,8 @@ class Card:
 
     def __eq__(self, other):
         """Compares between 2 cards"""
+        if type(other) != Card:
+            raise TypeError("Other must be a Card")
         if self.value == other.value:
             return True
         else:
--- game_card/test_Card.py
+++ game_card/test_Card.py
@@ -4,9 +4,55 @@ from unittest import TestCase
 
 class test_Card(TestCase):
     def setUp(self):
-        self.card = Card(5, 3)
+        self.card1 = Card(13, 4)
+        self.card2 = Card(1, 1)
 
     def test_init_valid(self):
         """Test case: valid input"""
-        self.assertEqual(5, self.card.value)
-        self.assertEqual(3, self.card.suit)
+        self.assertEqual(13, self.card1.value)
+        self.assertEqual(4, self.card1.suit)
+        self.assertEqual(1, self.card2.value)
+        self.assertEqual(1, self.card2.suit)
+
+    def test_init_invalid(self):
+        """Test case: invalid input"""
+        with self.assertRaises(ValueError):
+            card = Card(0, 3)
+        with self.assertRaises(ValueError):
+            card = Card(14, 3)
+        with self.assertRaises(ValueError):
+            card = Card(7, 0)
+        with self.assertRaises(ValueError):
+            card = Card(7, 5)
+
+    def test_gt_valid(self):
+        """Test case valid inputs"""
+        # Test case with Ace
+        self.assertFalse(self.card1 > self.card2)
+        # Test case same Value: 8 of diamond VS 8 of heart
+        self.assertTrue(Card(8, 3) > Card(8, 1))
+        # Test case same suit: 9 of diamond VS 8 of diamond
+        self.assertTrue(Card(9, 3) > Card(8, 3))
+        # Test case same Value(Ace): Ace of diamond VS Ace of heart
+        self.assertTrue(Card(1, 3) > Card(1, 1))
+        # Test case same Value and suit: 8 of diamond VS 8 of diamond
+        #self.assertn(Card(8, 3) > Card(8, 3))
+
+    def test_gt_invalid(self):
+        """Test case invalid: comparing between Card and NOT Card"""
+        with self.assertRaises(TypeError):
+            Card(5, 3).__gt__('card')
+
+    def test_eq_valid(self):
+        """Test case valid inputs"""
+        # Test same Values: True
+        self.assertTrue(Card(7, 3) == Card(7, 2))
+        # Test same Suits: False
+        self.assertFalse(Card(5, 3) == Card(7, 3))
+
+    def test_eq_invalid(self):
+        """Test case invalid: comparing between Card and NOT Card"""
+        with self.assertRaises(TypeError):
+            Card(5, 3).__eq__('card')
+
+
